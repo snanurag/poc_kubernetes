24f0ad80f4208d134ec39b00dd15b7cb645fac13
anurag shrinagar <anurag.shrinagar@oracle.com>
2019-07-15 18:33:32 +0530

    gke-svc-L4 load balancer

--- commands/install_ubuntu/auto_install.sh
+++ commands/install_ubuntu/auto_install.sh
@@ -1,6 +1,6 @@
 sudo apt-get update
 
-sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
