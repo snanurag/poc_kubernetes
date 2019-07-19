be895f08bf78d5a4298f81c976df265cac78b882
anurag shrinagar <anurag.shrinagar@oracle.com>
2019-07-19 15:19:29 +0530

    oci L4 trying to depict L7

--- commands/install_ol_centos_rhel/starting_master.sh
+++ commands/install_ol_centos_rhel/starting_master.sh
@@ -5,5 +5,5 @@ cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
 kubectl apply -f https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/rbac-kdd.yaml
 kubectl apply -f https://docs.projectcalico.org/v3.3/getting-started/kubernetes/installation/hosted/kubernetes-datastore/calico-networking/1.7/calico.yaml
-
+kubectl taint nodes --all node-role.kubernetes.io/master-
 
